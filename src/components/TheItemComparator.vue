bba1666e8b88ba68ff6865f05713e8ce1bcb42fb
marc-bouvier <m.bouvier.dev@gmail.com>
2017-11-25 19:20:21 +0100

    data for comparator

--- src/components/TheItemComparator.vue
+++ src/components/TheItemComparator.vue
@@ -15,7 +15,40 @@
 export default {
   name: 'the_item_comparator',
   data () {
-    return {}
+    return {
+      items: [
+        {
+          id: '1',
+          label: '',
+          brand: 'Crucial',
+          href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10217487',
+          features: [
+            {updateFor: 'Dell Latitude E6510'},
+            {driveSize: '2.5 inches'},
+            {driveTechnology: 'SSD'},
+            {driveCapacity: '120 Gb'},
+            {driveSerial: 'CT10217487'},
+            {driveSeries: 'BX300'},
+            {price: '61.19 €'}
+          ]
+        },
+        {
+          id: '2',
+          label: '',
+          brand: 'Crucial',
+          href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10134829',
+          features: [
+            {updateFor: 'Dell Latitude E6510'},
+            {driveSize: '2.5 inches'},
+            {driveTechnology: 'SSD'},
+            {driveCapacity: '240 Gb'},
+            {driveSerial: 'CT10134829'},
+            {driveSeries: 'BX300'},
+            {price: '89.99 €'}
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
