7a0d664879a39c2ca923cf201608f4db2078b6ee
marc-bouvier <m.bouvier.dev@gmail.com>
2017-11-25 19:04:33 +0100

    first commit : mocks

---
+++ src/components/TheAdmin.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+    Admin
+    <div><button >New item</button></div>
+    <div><button>Add feature</button></div>
+    <div><label>Feature</label>
+      <select>
+        <option></option>
+        <option>Feature</option>
+      </select>
+    </div>
+    <div>
+      <label>New feature type</label><input type="checkbox"/>
+      <label>key</label><input type="text"/>
+      <label>name</label><input type="text"/>
+      <label>type</label><select><option>String</option><option>Euro</option></select>
+    </div>
+    <div>
+      <label>value</label><input type="text"/>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'the_admin',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
---
+++ src/components/TheItemComparator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+  The comparator
+    <table>
+    <tbody>
+    <tr>    <td>Item 1</td><td>Item 2</td>    </tr>
+    <tr>    <td>10€</td><td>12€</td>          </tr>
+    <tr>    <td>1kg</td><td>1.5kg</td>        </tr>
+    </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'the_item_comparator',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped></style>
