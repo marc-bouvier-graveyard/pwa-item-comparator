3d81370ecd60bdd5cce86917aeb2cf54c564938d
marc-bouvier <m.bouvier.dev@gmail.com>
2017-11-27 13:44:18 +0100

    mapping des données pour le comparateur

--- src/components/TheItemComparator.vue
+++ src/components/TheItemComparator.vue
@@ -14,6 +14,47 @@
 <script>
 export default {
   name: 'the_item_comparator',
+  computed: {
+    tableRows (selectedItems) {
+      // Première ligne : nom du produit
+      return {
+        headerColumn: this.headerFromItems(selectedItems),
+        itemColumns: []
+      }
+    }
+  },
+  methods: {
+    headerFromItems (selectedItems) {
+      let columns = ['']
+      for (let item of selectedItems) {
+        columns.push({
+          label: item.label,
+          href: item.href,
+          price: item.price
+        })
+      }
+    },
+    rowsFromItems (selectedItems) {
+      let rows = []
+      let featureSet = this.featureSet()
+      for (let featureKey of featureSet) {
+        rows.push(this.rowFromItem(selectedItems, featureKey))
+      }
+      return rows
+    },
+    rowFromItem (selectedItems, featureKey) {
+      // test
+    },
+    featureSet (items) {
+      let featureSet = new Set()
+      for (let item of items) {
+        for (let featureKey of Object.keys(item.features)) {
+          featureSet.add(featureKey)
+        }
+      }
+      return featureSet
+    }
+  },
   data () {
     return {
       items: [
@@ -22,14 +63,14 @@ export default {
           label: '',
           brand: 'Crucial',
           href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10217487',
+          price: '61.19 €',
           features: [
             {updateFor: 'Dell Latitude E6510'},
             {driveSize: '2.5 inches'},
             {driveTechnology: 'SSD'},
             {driveCapacity: '120 Gb'},
             {driveSerial: 'CT10217487'},
-            {driveSeries: 'BX300'},
-            {price: '61.19 €'}
+            {driveSeries: 'BX300'}
           ]
         },
         {
@@ -37,14 +78,14 @@ export default {
           label: '',
           brand: 'Crucial',
           href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10134829',
+          price: '89.99 €',
           features: [
             {updateFor: 'Dell Latitude E6510'},
             {driveSize: '2.5 inches'},
             {driveTechnology: 'SSD'},
             {driveCapacity: '240 Gb'},
             {driveSerial: 'CT10134829'},
-            {driveSeries: 'BX300'},
-            {price: '89.99 €'}
+            {driveSeries: 'BX300'}
           ]
         }
       ]
