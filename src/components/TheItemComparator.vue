65e029370a0c3d09bf18607963f567293c4651f8
marc-bouvier <m.bouvier.dev@gmail.com>
2017-11-28 00:20:15 +0100

    comparator data layout

--- src/components/TheItemComparator.vue
+++ src/components/TheItemComparator.vue
@@ -3,95 +3,110 @@
   The comparator
     <table>
     <tbody>
-    <tr>    <td>Item 1</td><td>Item 2</td>    </tr>
-    <tr>    <td>10€</td><td>12€</td>          </tr>
-    <tr>    <td>1kg</td><td>1.5kg</td>        </tr>
+    <tr><td  v-for="item of tableRows.rowHeader"  >
+      <a :href="item.url">{{ item.label }}</a>
+     {{item.price}}
+      </td></tr>
+    <tr v-for="rowFeature of tableRows.rowFeatures"> 
+      <td v-for="featureCell of rowFeature">{{featureCell}}</td>          </tr>
+    
     </tbody>
     </table>
   </div>
+  
 </template>
 
 <script>
 export default {
-  name: 'the_item_comparator',
+  name: "the_item_comparator",
   computed: {
-    tableRows (selectedItems) {
+    tableRows() {
       // Première ligne : nom du produit
       return {
-        headerColumn: this.headerFromItems(selectedItems),
-        itemColumns: []
-      }
+        rowHeader: this.headerFromItems(this.items),
+        rowFeatures: this.rowsFromItems(this.items)
+      };
     }
   },
   methods: {
-    headerFromItems (selectedItems) {
-      let columns = ['']
+    headerFromItems(selectedItems) {
+      let rowHeader = [{}];
       for (let item of selectedItems) {
-        columns.push({
+        rowHeader.push({
           label: item.label,
-          href: item.href,
+          url: item.url,
           price: item.price
-        })
+        });
       }
+      return rowHeader;
     },
-    rowsFromItems (selectedItems) {
-      let rows = []
-      let featureSet = this.featureSet()
+    rowsFromItems(selectedItems) {
+      let rows = [];
+      let featureSet = this.featureSet(selectedItems);
       for (let featureKey of featureSet) {
-        rows.push(this.rowFromItem(selectedItems, featureKey))
+        rows.push(this.rowFromItem(selectedItems, featureKey));
       }
-      return rows
+      console.log(rows);
+      return rows;
     },
-    rowFromItem (selectedItems, featureKey) {
-      // test
-    },
-    featureSet (items) {
-      let featureSet = new Set()
-      for (let item of items) {
-        for (let featureKey of Object.keys(item.features)) {
-          featureSet.add(featureKey)
+    rowFromItem(selectedItems, featureKey) {
+      let row = [featureKey];
+      for (const item of selectedItems) {
+        if (Object.keys(item.features).includes(featureKey)) {
+          row.push(item.features[featureKey]);
+        } else {
+          row.push({});
         }
       }
-      return featureSet
+      return row;
+    },
+    featureSet(items) {
+      let featureSet = new Set();
+      for (let item of items) {
+        for (let featureKey of Object.keys(item.features)) {
+          featureSet.add(featureKey);
+        }
+      }
+      return featureSet;
     }
   },
-  data () {
+  data() {
     return {
       items: [
         {
-          id: '1',
-          label: '',
-          brand: 'Crucial',
-          href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10217487',
-          price: '61.19 €',
-          features: [
-            {updateFor: 'Dell Latitude E6510'},
-            {driveSize: '2.5 inches'},
-            {driveTechnology: 'SSD'},
-            {driveCapacity: '120 Gb'},
-            {driveSerial: 'CT10217487'},
-            {driveSeries: 'BX300'}
-          ]
+          id: "1",
+          label: "SSD Crucial BX300 120 Go (2,5 pouces / 7mm)",
+          brand: "Crucial",
+          url: "http://www.crucial.fr/fra/fr/latitude-e6510/CT10217487",
+          price: "61.19 €",
+          features: {
+            updateFor: "Dell Latitude E6510",
+            driveSize: "2.5 inches",
+            driveTechnology: "SSD",
+            driveCapacity: "120 Gb",
+            driveSerial: "CT10217487",
+            driveSeries: "BX300"
+          }
         },
         {
-          id: '2',
-          label: '',
-          brand: 'Crucial',
-          href: 'http://www.crucial.fr/fra/fr/latitude-e6510/CT10134829',
-          price: '89.99 €',
-          features: [
-            {updateFor: 'Dell Latitude E6510'},
-            {driveSize: '2.5 inches'},
-            {driveTechnology: 'SSD'},
-            {driveCapacity: '240 Gb'},
-            {driveSerial: 'CT10134829'},
-            {driveSeries: 'BX300'}
-          ]
+          id: "2",
+          label: "SSD Crucial BX300 240 Go (2,5 pouces / 7mm)",
+          brand: "Crucial",
+          url: "http://www.crucial.fr/fra/fr/latitude-e6510/CT10134829",
+          price: "89.99 €",
+          features: {
+            updateFor: "Dell Latitude E6510",
+            driveSize: "2.5 inches",
+            driveTechnology: "SSD",
+            driveCapacity: "240 Gb",
+            driveSerial: "CT10134829",
+            driveSeries: "BX300"
+          }
         }
       ]
-    }
+    };
   }
-}
+};
 </script>
 
 <style scoped></style>
